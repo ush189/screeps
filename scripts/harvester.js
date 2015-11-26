0eeb2e2995c763dc5a97ce79e84954acc09f6fb2
ush189 <github@ush189.de>
2015-11-26 21:02:32 +0100

    order sources by range

--- scripts/harvester.js
+++ scripts/harvester.js
@@ -2,9 +2,9 @@ module.exports = function (creep) {
     var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
 
     if (creep.carry.energy < creep.carryCapacity) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
+        var source = spawn.pos.findClosestByRange(FIND_SOURCES);
+        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
         }
     } else {
         if (creep.transferEnergy(spawn) == ERR_NOT_IN_RANGE) {
--- scripts/harvester2ndSrc.js
+++ scripts/harvester2ndSrc.js
@@ -2,9 +2,11 @@ module.exports = function (creep) {
     var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
 
     if (creep.carry.energy < creep.carryCapacity) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[1]);
+        var source = _.sortBy(spawn.room.find(FIND_SOURCES), function(source) {
+            return spawn.pos.getRangeTo(source.pos);
+        }).pop();
+        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(source);
         }
     } else {
         var extension = _.filter(creep.room.find(FIND_MY_STRUCTURES), function(myStructure) {
