2ce4cf49d77423ffaa7d9b951892cf16fd952f63
ush189 <github@ush189.de>
2015-11-28 18:34:28 +0100

    improved harvester2ndSrc

--- scripts/harvester2ndSrc.js
+++ scripts/harvester2ndSrc.js
@@ -1,25 +1,26 @@
 module.exports = function (creep) {
     var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
+    var source = _.sortBy(spawn.room.find(FIND_SOURCES), function(source) {
+        return spawn.pos.getRangeTo(source.pos);
+    }).pop();
 
-    if (creep.carry.energy < creep.carryCapacity) {
-        var source = _.sortBy(spawn.room.find(FIND_SOURCES), function(source) {
-            return spawn.pos.getRangeTo(source.pos);
-        }).pop();
+    if (creep.carry.energy < 50 || creep.pos.isNearTo(source) && creep.carry.energy < creep.carryCapacity) {
         if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
             creep.moveTo(source);
         }
     } else {
-        var extension = _.filter(creep.room.find(FIND_MY_STRUCTURES), function(myStructure) {
+        var extensions = _.filter(creep.room.find(FIND_MY_STRUCTURES), function(myStructure) {
             return myStructure.structureType === STRUCTURE_EXTENSION && myStructure.energy < myStructure.energyCapacity;
-        }).pop();
+        });
 
-        if (extension) {
-            if (creep.transferEnergy(extension) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(extension);
-            }
-        } else if (spawn.energy < spawn.energyCapacity) {
-            if (creep.transferEnergy(spawn) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(spawn);
+
+        if (extensions.length) {
+            var nearestExtension = _.sortBy(extensions, function(extension) {
+                return creep.pos.getRangeTo(extension.pos);
+            }).shift();
+
+            if (creep.transferEnergy(nearestExtension) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(nearestExtension);
             }
         } else {
             var storage = _.filter(creep.room.find(FIND_MY_STRUCTURES), function(myStructure) {
